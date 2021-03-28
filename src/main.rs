938bf12b9790d960ede13cbefcaee44c1d3c03f6
Bohun Kim <hoppang@gmail.com>
2021-03-28 01:22:59 -0700

    #2 read text file, and print it
    - from command line argument
    - does as_str copy whole string?

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 extern crate ncurses;
 
 use ncurses::*;
+use std::env;
+use std::fs;
 
-fn main()
-{
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    let mut contents = String::from("");
+    if args.len() == 2 {
+        let filename = args[1].as_str();
+        contents = fs::read_to_string(filename).expect("error");
+    }
+
     initscr();
-    addstr("Hello, world!");
+    addstr(contents.as_str());
     refresh();
     getch();
     endwin();
