1a91146ff1ba76a350b7c473442593b4db1959bf
Bohun Kim <hoppang@gmail.com>
2021-10-06 22:30:37 +0900

    아무키나 눌러서 종료

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,21 @@ pub use crossterm::{
     Command, Result,
 };
 
+fn read_char() -> Result<char> {
+    loop {
+        if let Ok(Event::Key(KeyEvent {code: KeyCode::Char(c), ..})) = event::read() {
+            return Ok(c);
+        }
+    }
+}
+
 fn run<W>(w: &mut W) -> Result<()> where W: Write {
     execute!(w, terminal::EnterAlternateScreen)?;
     terminal::enable_raw_mode()?;
+
+    match read_char()? {
+        _ => {}
+    }
 
     execute!(w, terminal::LeaveAlternateScreen)?;
     terminal::disable_raw_mode()
