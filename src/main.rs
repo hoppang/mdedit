4eb4036ae2e18b107defd23013336b26e3b18348
Bohun Kim <hoppang@gmail.com>
2021-10-06 22:28:38 +0900

    crossterm 추가 + 그냥 초기화 후 종료

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Write};
+pub use crossterm::{
+    cursor,
+    event::{self, Event, KeyCode, KeyEvent},
+    execute, queue, style,
+    terminal::{self, ClearType},
+    Command, Result,
+};
+
+fn run<W>(w: &mut W) -> Result<()> where W: Write {
+    execute!(w, terminal::EnterAlternateScreen)?;
+    terminal::enable_raw_mode()?;
+
+    execute!(w, terminal::LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()
+}
+
+fn main() -> Result<()> {
+    let mut stdout = io::stdout();
+    run(&mut stdout)
 }
