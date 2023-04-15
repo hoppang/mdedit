31a5a6a3be05ec5c76d9f04749733ab42a027aa0
Bohun Kim <hoppang@gmail.com>
2023-04-15 22:16:16 +0900

    #3 커서를 위/아래로 이동했을 때 x 위치 보정

--- src/editor/mod.rs
+++ src/editor/mod.rs
@@ -70,26 +70,14 @@ impl Editor {
                     self.cursor.x = self.current_line().width() as u16;
                     self.refresh(true)
                 }
-                (KeyModifiers::NONE, KeyCode::Left) => {
-                    self.current_line().prev();
-                    let char_width =
-                        self.contents[self.cursor.y as usize].current_char_width() as u16;
-                    self.cursor.move_left(char_width);
-                    self.refresh(false)
-                }
-                (KeyModifiers::NONE, KeyCode::Right) => {
-                    let char_width =
-                        self.contents[self.cursor.y as usize].current_char_width() as u16;
-                    self.cursor.move_right(char_width);
-                    let _no_use = self.current_line().next();
-                    self.refresh(false)
-                }
                 (KeyModifiers::NONE, KeyCode::Enter) => {
                     self.contents.push(LineBuffer::new());
                     self.cursor.x = 0;
                     self.cursor.y += 1;
                     self.refresh(false)
                 }
+                (KeyModifiers::NONE, KeyCode::Left) => self.handle_leftkey(true),
+                (KeyModifiers::NONE, KeyCode::Right) => self.handle_rightkey(true),
                 (KeyModifiers::NONE, KeyCode::Up) => self.handle_upkey(),
                 (KeyModifiers::NONE, KeyCode::Down) => self.handle_downkey(),
                 _ => {} // do nothing
@@ -109,6 +97,10 @@ impl Editor {
         if draw_line {
             self.current_line().draw(screen_width());
         }
+
+        // 디버그 정보 출력
+        self.print_dbgmsg();
+
         queue!(&self.screen, cursor::MoveTo(self.cursor.x, self.cursor.y))
             .expect("Failed to move cursor");
 
@@ -118,6 +110,15 @@ impl Editor {
                 panic!("Failed to put char {:?}", error);
             }
         };
+    }
+
+    fn print_dbgmsg(&mut self) {
+        queue!(&self.screen, cursor::MoveTo(0, screen_height() - 1))
+            .expect("Failed to move cursor");
+        
+        let x = self.cursor.x;
+        let y = self.cursor.y;
+        print!("current_line: {:?} cx {:?} cy {}    ", self.current_line(), x, y);
     }
 
     fn current_line(&mut self) -> &mut LineBuffer {
@@ -131,18 +132,59 @@ impl Editor {
         if self.cursor.y > 0 {
             self.cursor.y -= 1;
 
-            if self.cursor.x > self.current_line().len() as u16 {
-                self.cursor.x = self.current_line().len() as u16;
-            }
+            let x = self.calibrate_x();
+            self.cursor.x = x;
+            self.current_line().set_cursor(x);
 
             self.refresh(false);
         }
+
+        self.refresh(false);
     }
 
     fn handle_downkey(&mut self) {
         if self.contents.len() - 1 > self.cursor.y as usize {
             self.cursor.y += 1;
+
+            let x = self.calibrate_x();
+            self.cursor.x = x;
+            self.current_line().set_cursor(x);
+
             self.refresh(false);
+        }
+    }
+
+    /**
+        다른 라인으로 넘어갔을 때 x위치 보정
+        한글 등 너비가 1이 아닌 문자들이 있을 수도 있기 때문
+    */
+    fn calibrate_x(&mut self) -> u16 {
+        if self.cursor.x > self.current_line().len() as u16 {
+            self.cursor.x = self.current_line().len() as u16;
+        }
+
+        let x = self.cursor.x;
+        let new_x = self.current_line().index_from_width(x);
+        new_x
+    }
+
+    fn handle_leftkey(&mut self, refresh: bool) {
+        self.current_line().prev();
+        let char_width = self.current_line().current_char_width() as u16;
+        self.cursor.move_left(char_width);
+
+        if refresh {
+            self.refresh(false)
+        }
+    }
+
+    fn handle_rightkey(&mut self, refresh: bool) {
+        let char_width = self.current_line().current_char_width() as u16;
+        self.cursor.move_right(char_width);
+        let _no_use = self.current_line().next();
+
+        if refresh {
+            self.refresh(false)
         }
     }
 }
@@ -163,6 +205,13 @@ fn read_char() -> Result<(KeyModifiers, KeyCode)> {
 fn screen_width() -> usize {
     match size() {
         Ok((cols, _rows)) => cols as usize,
+        Err(error) => panic!("screen_width: {:?}", error),
+    }
+}
+
+fn screen_height() -> u16 {
+    match size() {
+        Ok((_cols, rows)) => rows,
         Err(error) => panic!("screen_width: {:?}", error),
     }
 }
