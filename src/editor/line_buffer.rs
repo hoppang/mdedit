a7158b516207a1accc616d751edf3c56c0cb8f35
Bohun Kim <hoppang@gmail.com>
2023-04-16 12:50:40 +0900

    #7 LineBuffer 의 cursor 이름이 혼란스러워서 byte_index 로 변경

--- src/editor/line_buffer.rs
+++ src/editor/line_buffer.rs
@@ -14,7 +14,7 @@ impl std::cmp::PartialEq for LineErr {
 
 pub struct LineBuffer {
     s: String,
-    cursor: usize,
+    byte_index: usize,
 }
 
 impl LineBuffer {
@@ -25,7 +25,7 @@ impl LineBuffer {
     pub fn new() -> LineBuffer {
         LineBuffer {
             s: String::new(),
-            cursor: 0,
+            byte_index: 0,
         }
     }
 
@@ -33,7 +33,7 @@ impl LineBuffer {
     pub fn from(arg: &str) -> LineBuffer {
         LineBuffer {
             s: String::from(arg),
-            cursor: 0,
+            byte_index: 0,
         }
     }
 
@@ -59,7 +59,7 @@ impl LineBuffer {
     }
 
     pub fn current_char(&self) -> char {
-        self.get_char(self.cursor)
+        self.get_char(self.byte_index)
     }
 
     fn get_char(&self, index: usize) -> char {
@@ -94,7 +94,13 @@ impl LineBuffer {
 
     pub fn push(&mut self, ch: char) {
         self.s.push(ch);
-        self.cursor = self.s.len();
+        self.byte_index = self.s.len();
+    }
+
+    #[cfg(test)]
+    pub fn push_str(&mut self, s: &str) {
+        self.s.push_str(s);
+        self.byte_index = self.s.len();
     }
 
     pub fn pop(&mut self) {
@@ -103,77 +109,82 @@ impl LineBuffer {
 
     pub fn next(&mut self) -> Result<usize, LineErr> {
         loop {
-            self.cursor += 1;
-            self.cursor = cmp::min(self.cursor, self.s.len());
+            self.byte_index += 1;
+            self.byte_index = cmp::min(self.byte_index, self.s.len());
 
-            if self.cursor >= self.s.len() {
+            if self.byte_index >= self.s.len() {
                 break Err(LineErr::EndOfString);
             }
 
-            if self.s.is_char_boundary(self.cursor) {
-                break Ok(self.cursor);
+            if self.s.is_char_boundary(self.byte_index) {
+                break Ok(self.byte_index);
             }
         }
     }
 
     pub fn prev(&mut self) -> usize {
         loop {
-            if self.cursor == 0 {
+            if self.byte_index == 0 {
                 break;
             }
 
-            self.cursor -= 1;
-            if self.cursor >= self.s.len() {
+            self.byte_index -= 1;
+            if self.byte_index >= self.s.len() {
                 continue;
             }
 
-            if self.s.is_char_boundary(self.cursor) {
+            if self.s.is_char_boundary(self.byte_index) {
                 break;
             }
         }
 
-        self.cursor
+        self.byte_index
     }
 
     /**
-        Return: (byte_cursor, screen_cursor)
+        이동키로 커서를 움직였을 때 새로운 byte index 설정이 필요하다.
+        Return: (new_byte_index, new_screen_x_pos)
     */
-    pub fn set_cursor(&mut self, new_cursor: u16) -> (u16, u16) {
-        self.cursor = new_cursor as usize;
+    pub fn set_byte_index(&mut self, new_byte_index: u16) -> (u16, u16) {
+        self.byte_index = new_byte_index as usize;
 
         loop {
-            if self.s.is_char_boundary(self.cursor) {
+            if self.s.is_char_boundary(self.byte_index) {
                 break;
             }
 
-            if self.cursor > 0 {
-                self.cursor -= 1;
+            if self.byte_index > 0 {
+                self.byte_index -= 1;
             }
         }
 
-        let head_width = self.s[..self.cursor].width_cjk();
-        (self.cursor as u16, head_width as u16)
+        let head_width = self.s[..self.byte_index].width_cjk();
+        (self.byte_index as u16, head_width as u16)
     }
 
     // width 는 화면상의 길이, length 는 바이트 단위 길이
     pub fn index_from_width(&self, width: u16) -> u16 {
-        let mut cursor: u16 = 0;
+        let mut byte_index: u16 = 0;
 
         for c in self.s.chars() {
-            if cursor + 1 >= width {
+            if byte_index + 1 >= width {
                 break;
             }
 
-            cursor += c.width_cjk().unwrap() as u16;
+            byte_index += c.width_cjk().unwrap() as u16;
         }
 
-        cursor
+        byte_index
+    }
+
+    pub fn get_byte_index(&self) -> usize {
+        self.byte_index
     }
 }
 
 impl fmt::Debug for LineBuffer {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "(\"{}\", {})", self.s, self.cursor)
+        write!(f, "(\"{}\", {})", self.s, self.byte_index)
     }
 }
 
@@ -185,14 +196,14 @@ mod test {
     fn test_new() {
         let s: LineBuffer = LineBuffer::new();
         assert_eq!(s.s, "");
-        assert_eq!(s.cursor, 0);
+        assert_eq!(s.byte_index, 0);
     }
 
     #[test]
     fn test_from() {
         let s: LineBuffer = LineBuffer::from("안녕");
         assert_eq!(s.s, "안녕");
-        assert_eq!(s.cursor, 0);
+        assert_eq!(s.byte_index, 0);
     }
 
     #[test]
@@ -207,7 +218,7 @@ mod test {
         };
 
         assert_eq!(result, 1);
-        assert_eq!(s.cursor, s.len());
+        assert_eq!(s.byte_index, s.len());
 
         assert_eq!(s.prev(), 6);
         assert_eq!(s.prev(), 3);
@@ -238,19 +249,19 @@ mod test {
     }
 
     #[test]
-    fn test_cursor() {
+    fn test_byte_index() {
         let mut s1: LineBuffer = LineBuffer::from("안녕하세요");
         s1.prev();
         assert_eq!(s1.current_char(), '안');
 
         // 커서가 char_boundary 위치가 아니더라도(한글 입력 중에 그렇게 될 수 있음)
         // 현재 캐릭터 정보를 가져올 수 있어야 한다.
-        assert_eq!(s1.set_cursor(5), (3, 2));
-        assert_eq!(s1.set_cursor(10), (9, 6));
+        assert_eq!(s1.set_byte_index(5), (3, 2));
+        assert_eq!(s1.set_byte_index(10), (9, 6));
         assert_eq!(s1.current_char(), '세');
 
         let mut s2: LineBuffer = LineBuffer::from("Hello");
-        assert_eq!(s2.set_cursor(3), (3, 3));
+        assert_eq!(s2.set_byte_index(3), (3, 3));
     }
 
     #[test]
