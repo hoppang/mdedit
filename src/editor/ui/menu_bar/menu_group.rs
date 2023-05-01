3cc134f929bcd8d0b5aaad6a3843bbf669c1767c
Bohun Kim <hoppang@gmail.com>
2023-05-02 00:28:31 +0900

    #16 메뉴막대 더미를 또 다른 더미로 교체

--- src/editor/ui/menu_bar/menu_group.rs
+++ src/editor/ui/menu_bar/menu_group.rs
@@ -1,2 +1,16 @@
+use super::menu_item::MenuItem;
+
 #[derive(Debug)]
-pub struct MenuGroup {}
+pub struct MenuGroup {
+    pub name: String,
+    _items: Vec<MenuItem>,
+}
+
+impl MenuGroup {
+    pub fn new(group_name: &str) -> MenuGroup {
+        MenuGroup {
+            name: String::from(group_name),
+            _items: Vec::new(),
+        }
+    }
+}
--- src/editor/ui/menu_bar/mod.rs
+++ src/editor/ui/menu_bar/mod.rs
@@ -1,9 +1,11 @@
 mod menu_group;
+mod menu_item;
 
 use crossterm::style::{Color, ResetColor, SetBackgroundColor, SetForegroundColor};
 use crossterm::{cursor, queue};
 use log::info;
 use menu_group::MenuGroup;
+use std::convert::TryInto;
 use std::io::Stdout;
 
 pub struct MenuBar {
@@ -12,7 +14,19 @@ pub struct MenuBar {
 
 impl MenuBar {
     pub fn new() -> MenuBar {
-        MenuBar { groups: Vec::new() }
+        let mut menu_bar = MenuBar { groups: Vec::new() };
+
+        let file_group = MenuGroup::new("File");
+        menu_bar.add_group(file_group);
+
+        let help_group = MenuGroup::new("Help");
+        menu_bar.add_group(help_group);
+
+        menu_bar
+    }
+
+    pub fn add_group(&mut self, new_group: MenuGroup) {
+        self.groups.push(new_group);
     }
 
     pub fn draw(&self, mut screen: &Stdout, width: usize) {
@@ -30,8 +44,12 @@ impl MenuBar {
             print!(" ");
         }
 
-        queue!(screen, cursor::MoveTo(4, 0)).unwrap();
-        print!("Dummy menubar");
+        for (iter, group) in self.groups.iter().enumerate() {
+            let x = (iter * 10 + 4).try_into().unwrap();
+            queue!(screen, cursor::MoveTo(x, 0)).unwrap();
+            print!("{}", group.name);
+        }
+
         queue!(screen, ResetColor).unwrap();
     }
 }
